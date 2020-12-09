ab126633d83304f9d7d75d889b216eaa33fb8c09
MoneyIgos <makowskiigor@gmail.com>
2020-12-09 19:42:38 +0100

    Fix time

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ export default {
     };
   },
   mounted() {
-    const cpRelease = new Date('Dec 10, 2020 00:00:00').getTime();
+    const cpRelease = new Date('Dec 10, 2020 01:00:00').getTime();
 
     setInterval(() => {
       const timeNow = new Date().getTime();
